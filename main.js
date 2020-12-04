348d77ba9cfe6f97b728576a038672a5d659e6e6
evans oginga <evansoginga7@gmail.com>
2020-12-04 12:32:45 +0300

    created the book class

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+    This book class represents a book
+*/
+class Book {
+    constructor(title, author, isbn, status) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+      this.status = status;
+    }
+  }
+  
